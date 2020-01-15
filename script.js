c6cd5db0394e075caf648bee8d315d484a2d427f
xsilvermc <57896691+xsilvermc@users.noreply.github.com>
2020-01-15 14:45:48 +0100

    zeug halt chill mal

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 class Character{
-	constructor(name, hp){
+	constructor(name, hp, xPos, yPos, id){
 		this.name = name;
 		this.hp = hp;
 		this.recharge = false;
+		this.x = xPos;
+		this.y = yPos;
+		this.id = id;
+		this.image = document.getElementById(id);
 	}
 }
 
-var player = new Character("player", 100);
+var player = new Character("player", 100, 50, 175, "player");
 
-var enemy1 = new Character("Dio", 100);
+var enemy1 = new Character("Dio", 100, 600, 100, "enemy1");
 
-var currentEnemy = enemy1;
+var enemy2 = new Character("jeff", 100, 600, 160, "enemy2");
+
+var enemy3 = new Character("john", 100, 650, 120, "enemy3");
+
+var enemy4 = new Character("fred", 100, 620, 65, "enemy4");
+
+var enemy5 = new Character("shaggy", 100, 590, 120, "enemy5");
+
+var enemy6 = new Character("scoob", 100, 640, 110, "enemy6");
+
+var currentEnemy = enemy6;
 
 var actionName;
 
 window.onload = function() {
+	//drawMap();
 	drawBattle(1);
 };
 
@@ -105,6 +120,7 @@ function newHP(target){
 }
 
 function turn(playerAction){
+	disableButtons();
 	if(!player.recharge){
 		switch (playerAction){
 			case punch:
@@ -150,12 +166,13 @@ function turn(playerAction){
 		document.getElementById('line2').innerHTML = "";
 	}, 3990);
 	setTimeout(function(){
-		document.getElementById('line1').innerHTML = 'What will player do?';
+		newTurn();
 	}, 4000);
 }
 
 function newTurn(){
 	document.getElementById('line1').innerHTML = 'What will player do?';
+	enableButtons();
 }
 
 function gameOver(){
@@ -169,6 +186,16 @@ function gameOver(){
 	document.getElementById('button2').setAttribute('onclick','reload()');
 	document.getElementById('button3').setAttribute('onclick','reload()');
 
+}
+
+function victory(){
+	var c = document.getElementById("mainCanvas");
+	var ctx = c.getContext("2d");
+	var text = document.getElementById("victory");
+	ctx.fillRect(0,0,900,500);
+	ctx.drawImage(text, 350, 235);
+	document.getElementById("line1").innerHTML = "Press any button to continue";
+	
 }
 
 function reload(){
@@ -185,13 +212,13 @@ var currentBattle = function(){
 	return parameters;
 }
 
-/*
 function disableButtons(){
-	document.getElementById('button1').setAttribute('onclick','console.log("do not get impatient now")');
-	document.getElementById('button2').setAttribute('onclick','console.log("do not get impatient now")');
-	document.getElementById('button3').setAttribute('onclick','console.log("do not get impatient now")');
+	document.getElementById("buttonDiv").style.display = "none";
 }
-*/
+
+function enableButtons(){
+	document.getElementById("buttonDiv").style.display = "block";
+}
 
 var dead = function(){
 	if(currentEnemy.hp == 0){
@@ -201,15 +228,55 @@ var dead = function(){
 	}
 }
 
-function drawBattle(battleNo){
+function drawEllipse(xPos){
 	var c = document.getElementById("mainCanvas");
 	var ctx = c.getContext("2d");
-	var player = document.getElementById("player");
+	ctx.save();
+	ctx.scale(1, 0.5);
+	ctx.beginPath();
+	ctx.arc(xPos, 830, 110, 0, Math.PI*2, false);
+	ctx.fillStyle = "#313131";
+	ctx.fill();
+	ctx.closePath();
+	ctx.restore();
+}
+
+function getCurrentEnemy(battleNo){
+	switch(battleNo){
+		case 1:
+			currentEnemy = enemy1;
+			break;
+		case 2:
+			currentEnemy = enemy2;
+			break;
+		case 3:
+			currentEnemy = enemy3;
+			break;
+		case 4:
+			currentEnemy = enemy4;
+			break;
+		case 5:
+			currentEnemy = enemy5;
+			break;
+		case 6:
+			currentEnemy = enemy6;
+			break;
+	}
+}
+
+function drawBattle(battleNo){
+	getCurrentEnemy(battleNo);
+	var c = document.getElementById("mainCanvas");
+	var ctx = c.getContext("2d");
+	var playerImage = document.getElementById("player");
 	var bg = document.getElementById("bg");
-	var enemy = document.getElementById("enemy1");
+	var enemyImage = document.getElementById(currentEnemy.id);
+	document.getElementById("enemyFace").src = enemyImage.src;
 	ctx.drawImage(bg, 0, 0);
-	ctx.drawImage(player, 50, 150);
-	ctx.drawImage(enemy, 600, 100);
+	drawEllipse(170);
+	drawEllipse(700);
+	ctx.drawImage(playerImage, player.x, player.y);
+	ctx.drawImage(enemyImage, currentEnemy.x, currentEnemy.y);
 	document.getElementById('button1').setAttribute('onclick','turn(punch)');
 	document.getElementById('button2').setAttribute('onclick','turn(throwObject)');
 	document.getElementById('button3').setAttribute('onclick','turn(punchBarrage)');
@@ -223,4 +290,61 @@ function changeButtonColor(colorIndex){
 		document.getElementById(targetButton).style.borderColor = buttonBorderColor[colorIndex];	
 	}
 	console.log("buttons changed");
+}
+
+function drawMap(){
+	var map = document.getElementById("map");
+	var path = document.getElementById("path");
+	var castle = document.getElementById("castle");
+	var bottomY = 505;
+	var middleY = 256;
+	var topY = 9;
+	var leftX = 169;
+	var midLeftX = 510;
+	var midRightX = 825;
+	var rightX = 1140;
+	var c = document.getElementById("mapCanvas");
+	var ctx = c.getContext("2d");
+	ctx.drawImage(map,0,0);
+	ctx.drawImage(path,0,0);
+	ctx.drawImage(castle,midLeftX,bottomY);
+	ctx.drawImage(castle,rightX,bottomY);
+	ctx.drawImage(castle,leftX,middleY);
+	ctx.drawImage(castle,midRightX,middleY);
+	ctx.drawImage(castle,midLeftX,topY);
+	ctx.drawImage(castle,rightX,topY);
+}
+
+function outputXY(){
+	var x = event.clientX;     // Get the horizontal coordinate
+	var y = event.clientY;     // Get the vertical coordinate
+	var coor = "X coords: " + x + ", Y coords: " + y;
+	console.log(coor);
+}
+
+var getX = function(){
+	var x = event.clientX;
+	return x;
+}
+
+var getY = function(){
+	var y = event.clientY;
+	return y;
+}
+
+function levelSelect(){
+	var bottomY = 505;
+	var middleY = 256;
+	var topY = 9;
+	var leftX = 169;
+	var midLeftX = 510;
+	var midRightX = 825;
+	var rightX = 1140;
+	var mouseX = getX();
+	var mouseY = getY();
+	if((mouseX > midLeftX)&&(mouseX < (midLeftX + 128)) && (mouseY > bottomY) && (mouseY < (bottomY + 128))){
+		alert("level 1 starts now");
+	}else{
+		alert("X coords: " + mouseX + ", Y coords: " + mouseY);
+	}
 }
